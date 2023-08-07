b003e8943f1cbedb18a968648a16b7c4bd35e70d
AaronPrince <princeborn1999@gmail.com>
2023-08-07 13:44:40 +0800

    set project is react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import HeaderComponent from './components/Header';
+function App() {
+  return (
+    <div className="App">
+      <HeaderComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function HeaderComponent() {
+    return(
+        <div>
+            <header>this is header test</header>
+        </div>
+    )
+}
+export default HeaderComponent
