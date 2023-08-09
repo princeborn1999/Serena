b1792ec918335c6baadeeeb17abbc3637c676c4f
AaronPrince <princeborn1999@gmail.com>
2023-08-09 15:24:19 +0800

    add routing

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './components/Header';
 import NavComponent from './components/Nav';
-import ControlComponent from './components/Control';
+import ControlComponent from './components/Control/Control';
 import FooterComponent from './components/Footer';
+import { Link, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <HeaderComponent />
-      <div className='flex'>
+      <div className='flex h-screen'>
        <NavComponent />
-       <ControlComponent />
+       <Route exact path="/"><div>wait to come.</div></Route>
+       <Route path="/control"><ControlComponent /></Route>
+       <Route path="/template"><ControlComponent /></Route>
+       <Route path="/timeline"><ControlComponent /></Route>
+       <Route path="/api"><ControlComponent /></Route>
       </div>
       <FooterComponent />
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,24 @@
 import './Nav.css'
+import { Link } from "react-router-dom";
 function NavComponent() {
     return(
         <div className="bg-gray-700 p-5 nav-btn">
             <div>
-                <button>Introduction</button>
+                <button>
+                    <Link to="/">Introduction</Link>
+                </button>
             </div>
             <div>
-                <button>Control</button>
+                <button><Link to="/control">Control</Link></button>
             </div>
             <div>
-                <button>Template</button>
+                <button><Link to="/template">Template</Link></button>
             </div>
             <div>
-                <button>Timeline</button>
+                <button><Link to="/timeline">Timeline</Link></button>
             </div>
             <div>
-                <button>API</button>
+                <button><Link to="/api">API</Link></button>
             </div>
         </div>
     )
