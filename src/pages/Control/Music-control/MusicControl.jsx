ef55a24f27582c307417ca1898b61db33fab8b55
AaronPrince <princeborn1999@gmail.com>
2023-08-10 17:40:40 +0800

    install useSound

--- src/pages/Control/Music-control/MusicControl.jsx
+++ src/pages/Control/Music-control/MusicControl.jsx
@@ -1,11 +1,12 @@
-import Box from '@mui/material/Box';
+import useSound from 'use-sound';
+import angelical from '../../../assets/music/angelical-pad-143276.mp3'
 function MusicControl(){
+    const [play] = useSound(angelical);
     return(
             <div className='shadow-xl p-5 bg-white rounded-lg w-96'>
                 <h1 className='font-bold'>MUSIC TYPE</h1>
                 <div class="m-2 flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
-                    <input id="bordered-checkbox-1" type="checkbox" value="" name="bordered-checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                    <label for="bordered-checkbox-1" class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default radio</label>
+                    <button onClick={play}>Boop!</button>;    
                 </div>
                 <div class="m-2 flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
                     <input id="bordered-checkbox-1" type="checkbox" value="" name="bordered-checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
