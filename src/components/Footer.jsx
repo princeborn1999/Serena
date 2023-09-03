2114b3b4111395a1c0b9e32ab3144d7d1d220c15
Prince <princeborn1999@gmail.com>
2023-09-03 21:20:20 +0800

    delete music part

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
 function FooterComponent() {
-    return(
-        <div>
-            <hr className="bg-gray-200"></hr>
-            <div className="py-5 px-10 flex items-center justify-center ">
-                <p label='zone-title' 
-                className="font-bold text-2xl text-arduinoReact 
-                           drop-shadow-2xl shadow-blue-500/50">ZONE</p>
-                <p className="pl-5">©All copyright reserved by AaronPrince.</p>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <hr className="bg-gray-200"></hr>
+      <div className="py-5 px-10 flex items-center justify-center ">
+        <p
+          label="zone-title"
+          className="font-bold text-2xl text-arduinoReact 
+                           drop-shadow-2xl shadow-blue-500/50"
+        >
+          ZONO
+        </p>
+        <p className="pl-5">©All copyright reserved by AaronPrince.</p>
+      </div>
+    </div>
+  );
 }
-export default FooterComponent
+export default FooterComponent;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,74 @@
-import { useState } from 'react';
-import Modal from 'react-modal'
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { useState } from "react";
+import Modal from "react-modal";
+import React from "react";
+import ReactDOM from "react-dom";
 // import  SignInModal from './SignInModal'
 function HeaderComponent() {
-    const [modalState, setModalState] = useState(false);
-    const customStyles = {
-        content: {
-            width: '50%',
-            maxHeight: '80vh',
-            margin: 'auto'
-        }
-    };
-    const openModal = () => {
-        setModalState(true)
-    }
+  const [modalState, setModalState] = useState(false);
+  const customStyles = {
+    content: {
+      width: "50%",
+      maxHeight: "80vh",
+      margin: "auto",
+    },
+  };
+  const openModal = () => {
+    setModalState(true);
+  };
 
-    const closeModal = () => {
-        setModalState(false)
-    }
+  const closeModal = () => {
+    setModalState(false);
+  };
 
-    return(
-        <div className="relative">
-            <div className="fixed sticky top-0 left-0 right-0 p-2 shadow-sm bg-arduinoReact">
-                <header className="flex">
-                    <div className="flex pl-5 ">
-                        <h1 className="text-white font-bold text-3xl 
-                                       drop-shadow-2xl shadow-white-400/50">ZONE</h1>
-                    </div>
-                    <div className="flex w-full justify-end pr-5">
-                        <button className="text-white font-semibold" onClick={openModal}>sign in</button>
-                    </div>
-                    <Modal 
-                        isOpen={modalState} 
-                        onRequestClose={closeModal}
-                        style={customStyles}>
-                        <div className='flex justify-center'>
-                            <div>
-                                <div className='m-1'>
-                                    <label htmlFor="account">Account</label><input type="text" />
-                                </div>
-                                <div className='m-1'>
-                                <label htmlFor="password">Password</label><input type="text" />
-                                </div>
-                                <div className='p-5'>
-                                    <button className='m-1' onClick={closeModal}>Login</button>
-                                    <button className='m-1' onClick={closeModal}>Sign up</button>
-                                    <button className='m-1' onClick={closeModal}>Cancel</button>
-                                </div>
-                            </div>
-                        </div>    
-                    </Modal>
-                </header>
+  return (
+    <div className="relative">
+      <div className="fixed sticky top-0 left-0 right-0 p-2 shadow-sm bg-arduinoReact">
+        <header className="flex">
+          <div className="flex pl-5 ">
+            <h1
+              className="text-white font-bold text-3xl 
+                                       drop-shadow-2xl shadow-white-400/50"
+            >
+              ZONO
+            </h1>
+          </div>
+          <div className="flex w-full justify-end pr-5">
+            <button className="text-white font-semibold" onClick={openModal}>
+              sign in
+            </button>
+          </div>
+          <Modal
+            isOpen={modalState}
+            onRequestClose={closeModal}
+            style={customStyles}
+          >
+            <div className="flex justify-center">
+              <div>
+                <div className="m-1">
+                  <label htmlFor="account">Account</label>
+                  <input type="text" />
+                </div>
+                <div className="m-1">
+                  <label htmlFor="password">Password</label>
+                  <input type="text" />
+                </div>
+                <div className="p-5">
+                  <button className="m-1" onClick={closeModal}>
+                    Login
+                  </button>
+                  <button className="m-1" onClick={closeModal}>
+                    Sign up
+                  </button>
+                  <button className="m-1" onClick={closeModal}>
+                    Cancel
+                  </button>
+                </div>
+              </div>
             </div>
-        </div>
-    )
+          </Modal>
+        </header>
+      </div>
+    </div>
+  );
 }
-export default HeaderComponent
-
+export default HeaderComponent;
