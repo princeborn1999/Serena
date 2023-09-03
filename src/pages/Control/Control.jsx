bea09b62b914bb80e559c00a1384a9059d681cae
Prince <princeborn1999@gmail.com>
2023-09-03 21:39:27 +0800

    移除music

--- src/pages/Control/Control.jsx
+++ src/pages/Control/Control.jsx
@@ -1,48 +1,44 @@
-import * as React from 'react';
-import DegreeControl  from './Degree-control/DegreeControl';
-import MusicControl from './Music-control/MusicControl';
-import RainControl from './Rain-control/RainControl';
-import SmellControl from './Smell-control/SmellControl';
+import * as React from "react";
+import DegreeControl from "./Degree-control/DegreeControl";
+import RainControl from "./Rain-control/RainControl";
+import SmellControl from "./Smell-control/SmellControl";
 const marks = [
-    {
-      value: 0,
-      label: '0°C',
-    },
-    {
-      value: 20,
-      label: '20°C',
-    },
-    {
-      value: 37,
-      label: '37°C',
-    },
-    {
-      value: 100,
-      label: '100°C',
-    },
-  ];
+  {
+    value: 0,
+    label: "0°C",
+  },
+  {
+    value: 20,
+    label: "20°C",
+  },
+  {
+    value: 37,
+    label: "37°C",
+  },
+  {
+    value: 100,
+    label: "100°C",
+  },
+];
 
-  function valuetext(value) {
-    return `${value}°C`;
-  }
-  
-function ControlComponent() {
-    return(
-        //TODO alert"doesn't detect any arduino devices"
-        <div className='py-5 px-10'>   
-            <div className=''>
-              <DegreeControl />
-            </div>
-            <div className=''>
-              <MusicControl />
-            </div>
-            <div className=''>
-              <RainControl />
-            </div>
-            <div className=''>
-              <SmellControl />
-            </div>
-        </div>
-    )
+function valuetext(value) {
+  return `${value}°C`;
 }
-export default ControlComponent
+
+function ControlComponent() {
+  return (
+    //TODO alert"doesn't detect any arduino devices"
+    <div className="py-5 px-10">
+      <div className="">
+        <DegreeControl />
+      </div>
+      <div className="">
+        <RainControl />
+      </div>
+      <div className="">
+        <SmellControl />
+      </div>
+    </div>
+  );
+}
+export default ControlComponent;
